63b1ea020f06e23b47c9dc4f3272931719defe06
Kedrick <hill1588@morris.umn.edu>
2020-10-28 16:38:32 -0500

    create main method fermatPrimality

--- src/FermatPrimalityTest.java
+++ src/FermatPrimalityTest.java
@@ -1,2 +1,26 @@
+import java.util.Random;
+import java.lang.Math;
+
 public class FermatPrimalityTest {
+
+public static void fermatPrimality(int p, int s) {
+    Random rand = new Random();
+    for (int i = 1; i <= s; i++) {
+        int a = rand.nextInt(p-2) + 2;
+        if (Math.pow(a, (p-1)) != 1) {
+            System.out.println("p is composite");
+        }
+    }
+    System.out.println("p is likely prime");
 }
+
+
+
+
+
+
+    public static void main(String[] args) {
+    
+    }
+}
+
