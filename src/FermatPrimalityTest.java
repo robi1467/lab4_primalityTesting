9895a26e49cab3f2f66d3ae6e7659a693ae1e523
JayS <smit9025@morris.umn.edu>
2020-10-28 16:42:20 -0500

    Merge

--- src/FermatPrimalityTest.java
+++ src/FermatPrimalityTest.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 
 public class FermatPrimalityTest {
 
-    public void FermatPrimalityTest(){
-
+    static void fermatPrimality(int p, int s) {
+        Random rand = new Random();
+        for (int i = 1; i <= s; i++) {
+            int a = rand.nextInt(p - 2) + 2;
+            if (Math.pow(a, (p - 1)) != 1) {
+                System.out.println("p is composite");
+            }
+        }
+        System.out.println("p is likely prime");
     }
 
     public static void main(String[] args) {
@@ -15,20 +22,9 @@ public class FermatPrimalityTest {
         int n = myObj.nextInt();
         System.out.println("Please input the number of times to test.");
         int k = myObj.nextInt();
-        FermatPrimalityTest test = new FermatPrimalityTest();
-        test.fermatPrimality(n, k);
+        fermatPrimality(n, k);
 //        System.out.println("n: " + n);
 //        System.out.println("k: " + k);
     }
-
-public static void fermatPrimality(int p, int s) {
-    Random rand = new Random();
-    for (int i = 1; i <= s; i++) {
-        int a = rand.nextInt(p-2) + 2;
-        if (Math.pow(a, (p-1)) != 1) {
-            System.out.println("p is composite");
-        }
-    }
-    System.out.println("p is likely prime");
 }
 
